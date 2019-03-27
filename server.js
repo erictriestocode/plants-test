16e248b0ffb5be8c6f31c231b9b10ec65e58ceaf
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-26 23:12:44 -0500

    Added Multer per tutorial

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+// Bring in Reqs
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 
+// Set Multer as Storage Engine
+const storage = multer.diskStorage({
+    destination: "./public/upload/",
+    filename: function(req, file, cb){
+        // first parameter of the function is an error and null is supposed to be per docs(14:30)
+        // file.fieldname is the name on the input tag on index.html(imageUpload)
+        // Date.now() adds a timestamp at the time of upload
+        // path ext name will take the original extension and append to the new file name
+        cb(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname) );
+    }
+});
+
+// Initialize Upload
+const upload = multer({
+    storage: storage
+}).single("imageUpload");
+
+// Initialize express
 const app = express();
 const PORT = process.env.PORT || 5000;
 
