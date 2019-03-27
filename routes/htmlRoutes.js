b6881755496e92bb869356dce5f6de89cb19dcbe
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-27 00:25:49 -0500

    moved multer logic into routes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,23 @@
 const path = require("path");
+const multer = require("multer");
 // const upload = require("upload");
+
+// Set Multer as Storage Engine
+const storage = multer.diskStorage({
+    destination: "./public/upload/",
+    filename: function(req, file, cb){
+        // first parameter of the function is an error and null is supposed to be per docs(14:30)
+        // file.fieldname is the name on the input tag on index.html(imageUpload)
+        // Date.now() adds a timestamp at the time of upload
+        // path ext name will take the original extension and append to the new file name
+        cb(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname) );
+    }
+});
+
+// Initialize Upload
+const upload = multer({
+    storage: storage
+}).single("imageUpload");
 
 module.exports = function (app) {
 
@@ -11,7 +29,7 @@ module.exports = function (app) {
 
     // Image Upload Route
     app.post("/upload", function (req, res) {
-        res.send("test");
+        res.send("Uploading...");
         upload(req, res, function (err) {
             if (err) {
                 alert(err)
