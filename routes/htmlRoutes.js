738c90c95e80b53cdbe846bc1f9aee4d14d767b8
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-26 23:32:18 -0500

    broken image post code until multer export working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const upload = require("upload");
 
 module.exports = function(app) {
 
     // Home Route
-    app.get("/", function(req, res) {
+    app.get("/", function(req,res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     
+    // Image Upload Route
+    app.post("/upload", function(req,res){
+        res.send("test");
+        upload(req,res, function(err){
+            if(err){
+                alert(err)
+            } else{
+                console.log(req.file);
+                res.send("test");
+            }
+        })
+    });
+
   };
   
