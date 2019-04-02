8aa459494de9961b86a59bdde2f5e33b99390aa7
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-04-01 23:05:39 -0500

    Commented out stale file for deletion later

--- data/identification.js
+++ data/identification.js
@@ -1,31 +1,33 @@
+// *********************************************
+// STALE FILE! DO NOT USE!
+// *********************************************
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var plantApiKey = "uE6x4WzpWVPnWwBZqpVSneRhhl9y7gwe3hRB8qfUt5wpVpbDzF"; //needs to be replaced by env variable
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var plantApiKey = "uE6x4WzpWVPnWwBZqpVSneRhhl9y7gwe3hRB8qfUt5wpVpbDzF"; //needs to be replaced by env variable
-
-var requestTwo = new XMLHttpRequest();
+// var requestTwo = new XMLHttpRequest();
 
 
 
-module.exports = function (idNumber) {
+// module.exports = function (idNumber) {
 
-    // request.abort(); No idea why thi s isnt working
-    requestTwo.open('POST', 'https://api.plant.id/check_identifications');
-    requestTwo.setRequestHeader('Content-Type', 'application/json');
+//     // request.abort(); No idea why thi s isnt working
+//     requestTwo.open('POST', 'https://api.plant.id/check_identifications');
+//     requestTwo.setRequestHeader('Content-Type', 'application/json');
 
-    requestTwo.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            console.log(this);
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-        }
-    };
+//     requestTwo.onreadystatechange = function () {
+//         if (this.readyState === 4) {
+//             console.log(this);
+//             console.log('Status:', this.status);
+//             console.log('Headers:', this.getAllResponseHeaders());
+//             console.log('Body:', this.responseText);
+//         }
+//     };
 
-    var bodyTwo = {
-        'key': plantApiKey,
-        'ids': [idNumber]
-    };
+//     var bodyTwo = {
+//         'key': plantApiKey,
+//         'ids': [idNumber]
+//     };
 
-    requestTwo.send(JSON.stringify(bodyTwo));
-};
+//     requestTwo.send(JSON.stringify(bodyTwo));
+// };
 
